8416001d309b5b2be15c9ddd4f947fe5cf39ce3a
Michael Everett <mfe@mit.edu>
2019-07-08 22:35:03 -0400

    added flag to identify policies

--- gym_collision_avoidance/envs/policies/PPOPolicy.py
+++ gym_collision_avoidance/envs/policies/PPOPolicy.py
@@ -5,9 +5,11 @@ from gym_collision_avoidance.envs.policies.Policy import Policy
 class PPOPolicy(Policy):
     def __init__(self):
         Policy.__init__(self, str="PPO")
+        self.is_still_learning = True
 
     def network_output_to_action(self, agent, network_output):
         # network_output: [speed scaling btwn 0-1, max heading angle delta btwn 0-1]
+        
         heading = agent.max_heading_change*(2.*network_output[1] - 1.)
         speed = agent.pref_speed * network_output[0]
         actions = np.array([speed, heading])
--- gym_collision_avoidance/envs/policies/Policy.py
+++ gym_collision_avoidance/envs/policies/Policy.py
@@ -3,6 +3,7 @@
 class Policy(object):
     def __init__(self, str="NoPolicy"):
     	self.str = str
+    	self.is_still_learning = False
 
     def find_next_action(self, agents):
         raise NotImplementedError
