5032d273b39d99ab41a7454fdf028ea2b4ab0dce
Michael Everett <mfe@mit.edu>
2020-04-08 21:53:16 -0400

    add ability to provide external sensor args, make otheragentsstatessensor actually respect closest-Last type rules

--- gym_collision_avoidance/envs/sensors/OtherAgentsStatesSensor.py
+++ gym_collision_avoidance/envs/sensors/OtherAgentsStatesSensor.py
@@ -5,9 +5,38 @@ from gym_collision_avoidance.envs.util import compute_time_to_impact, vec2_l2_no
 import operator
 
 class OtherAgentsStatesSensor(Sensor):
-    def __init__(self):
+    def __init__(self, max_num_other_agents_observed=Config.MAX_NUM_OTHER_AGENTS_OBSERVED, agent_sorting_method=Config.AGENT_SORTING_METHOD):
         Sensor.__init__(self)
         self.name = 'other_agents_states'
+        self.max_num_other_agents_observed = max_num_other_agents_observed
+        self.agent_sorting_method = agent_sorting_method
+
+    def get_clipped_sorted_inds(self, sorting_criteria):
+        # Grab first N agents (where N=Config.MAX_NUM_OTHER_AGENTS_OBSERVED)
+        if self.agent_sorting_method in ['closest_last', 'closest_first']:
+            # where "first" == closest
+            sorted_sorting_criteria = sorted(sorting_criteria, key = lambda x: (x[1], x[2]))
+        elif self.agent_sorting_method in ['time_to_impact']:
+            # where "first" == lowest time-to-impact
+            sorted_sorting_criteria = sorted(sorting_criteria, key = lambda x: (-x[3], -x[1], x[2]))
+        clipped_sorting_criteria = sorted_sorting_criteria[:self.max_num_other_agents_observed]
+
+        # Then sort those N agents by the preferred ordering scheme
+        if self.agent_sorting_method == "closest_last":
+            # sort by inverse distance away, then by lateral position
+            sorted_dists = sorted(clipped_sorting_criteria, key = lambda x: (-x[1], x[2]))
+        elif self.agent_sorting_method == "closest_first":
+            # sort by distance away, then by lateral position
+            sorted_dists = sorted(clipped_sorting_criteria, key = lambda x: (x[1], x[2]))
+        elif self.agent_sorting_method == "time_to_impact":
+            # sort by time_to_impact, break ties by distance away, then by lateral position (e.g. in case inf TTC)
+            sorted_dists = sorted(clipped_sorting_criteria, key = lambda x: (-x[3], -x[1], x[2]))
+        else:
+            raise ValueError("Did not supply proper self.agent_sorting_method in Agent.py.")
+
+        clipped_sorted_inds = [x[0] for x in sorted_dists]
+        return clipped_sorted_inds
+
 
     def sense(self, agents, agent_index, top_down_map):
         host_agent = agents[agent_index]
@@ -32,32 +61,18 @@ class OtherAgentsStatesSensor(Sensor):
                 # print("Agent too far away")
                 continue
 
-            if Config.AGENT_SORTING_METHOD != "time_to_impact":
+            if self.agent_sorting_method != "time_to_impact":
                 time_to_impact = None
             else:
-                time_to_impact = compute_time_to_impact(self.pos_global_frame,
+                time_to_impact = compute_time_to_impact(host_agent.pos_global_frame,
                                                         other_agent.pos_global_frame,
-                                                        self.vel_global_frame,
+                                                        host_agent.vel_global_frame,
                                                         other_agent.vel_global_frame,
                                                         combined_radius)
 
             sorting_criteria.append([i, round(dist_2_other,2), p_orthog_ego_frame, time_to_impact])
 
-        if Config.AGENT_SORTING_METHOD == "closest_last":
-            # sort by inverse distance away, then by lateral position
-            sorted_dists = sorted(sorting_criteria, key = lambda x: (-x[1], x[2]))
-        elif Config.AGENT_SORTING_METHOD == "closest_first":
-            # sort by distance away, then by lateral position
-            sorted_dists = sorted(sorting_criteria, key = lambda x: (x[1], x[2]))
-        elif Config.AGENT_SORTING_METHOD == "time_to_impact":
-            # sort by time_to_impact, break ties by distance away, then by lateral position (e.g. in case inf TTC)
-            sorted_dists = sorted(sorting_criteria, key = lambda x: (-x[3], -x[1], x[2]))
-        else:
-            raise ValueError("Did not supply proper Config.AGENT_SORTING_METHOD in Agent.py.")
-
-
-        sorted_inds = [x[0] for x in sorted_dists]
-        clipped_sorted_inds = sorted_inds[-Config.MAX_NUM_OTHER_AGENTS_OBSERVED:]
+        clipped_sorted_inds = self.get_clipped_sorted_inds(sorting_criteria)
         clipped_sorted_agents = [agents[i] for i in clipped_sorted_inds]
 
         other_agents_states = np.zeros((Config.MAX_NUM_OTHER_AGENTS_OBSERVED, 7))
--- gym_collision_avoidance/envs/sensors/Sensor.py
+++ gym_collision_avoidance/envs/sensors/Sensor.py
@@ -4,3 +4,9 @@ class Sensor(object):
 
     def sense(self, agents, agent_index, top_down_map):
         raise NotImplementedError
+
+    def set_args(self, args):
+        # Supply a dict of arg key value pairs
+        for arg, value in args.items():
+            # print("Setting self.{} to {}".format(arg, value))
+            setattr(self, arg, value)
