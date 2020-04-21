d955b68f6fc0063e21044a7f6c1e261693db77fc
Michael Everett <mfe@mit.edu>
2020-04-21 17:54:47 -0400

    rm dead code from test suite

--- gym_collision_avoidance/experiments/src/run_full_test_suite.py
+++ gym_collision_avoidance/experiments/src/run_full_test_suite.py
@@ -10,22 +10,7 @@ from gym_collision_avoidance.envs import Config
 import gym_collision_avoidance.envs.test_cases as tc
 from gym_collision_avoidance.experiments.src.env_utils import run_episode, create_env, store_stats, policies
 
-# from gym_collision_avoidance.envs.sensors.LaserScanSensor import LaserScanSensor
-# from gym_collision_avoidance.envs.sensors.OtherAgentsStatesSensor import OtherAgentsStatesSensor
-
-# def main():
-#     np.random.seed(0)
-#     env, one_env = create_env()
-#     for num_agents in num_agents_to_test:
-#         for test_case in range(num_test_cases):
-
 def reset_env(env, one_env, test_case_fn, test_case_args, test_case, num_agents, policies, policy, prev_agents):
-    # if start_from_last_configuration:
-    #     if test_case == 0:
-    #         agents = tc.small_test_suite(num_agents=num_agents, test_case_index=0)
-    #     test_case_args['agents'] = agents
-    #     test_case_args['letter'] = letters[test_case % len(letters)]
-    # else:
     test_case_args['num_agents'] = num_agents
     test_case_args['test_case_index'] = test_case
     one_env.plot_policy_name = policy
@@ -48,54 +33,6 @@ def reset_env(env, one_env, test_case_fn, test_case_args, test_case, num_agents,
     init_obs = env.reset()
     one_env.test_case_index = test_case
     return init_obs
-
-# def reset_env2(env, one_env, test_case_fn, test_case_args, test_case, num_agents, policies, policy, prev_agents):
-#     test_case_fn = tc.get_testcase_crazy
-#     # test_case_fn = tc.get_testcase_random
-
-#     # test_case_args['policy_to_ensure'] = policies[policy]['policy']
-#     # test_case_args['policies'] = ['noncoop', policies[policy]['policy'], 'static']
-#     # test_case_args['policy_distr'] = [0.05, 0.9, 0.05]
-#     # test_case_args['speed_bnds'] = [0.5, 2.0]
-#     # test_case_args['radius_bnds'] = [0.2, 0.8]
-#     # test_case_args['side_length'] = [
-#     #         {'num_agents': [0,5], 'side_length': [4,5]}, 
-#     #         # {'num_agents': [5,np.inf], 'side_length': [6,8]},
-#     #         {'num_agents': [5,np.inf], 'side_length': [2,2]},
-#     #         ]
-
-#     test_case_args['policy'] = policies[policy]['policy']
-#     # test_case_args['num_agents'] = num_agents
-#     # test_case_args['test_case_index'] = test_case
-#     one_env.plot_policy_name = policy
-#     # if 'sensors' in policies[policy]:
-#     #     test_case_args['agents_sensors'] = policies[policy]['sensors']
-#     # else:
-#     #     test_case_args['agents_sensors'] = []
-#     # test_case_args['prev_agents'] = prev_agents
-#     agents = test_case_fn(**test_case_args)
-#     if prev_agents is None:
-#         for agent in agents:
-#             if 'checkpt_name' in policies[policy] and hasattr(agent.policy, "initialize_network"):
-#                 agent.policy.env = env
-#                 agent.policy.initialize_network(**policies[policy])
-#             if 'sensor_args' in policies[policy]:
-#                 for sensor in agent.sensors:
-#                     sensor.set_args(policies[policy]['sensor_args'])
-#     one_env.set_agents(agents)
-#     init_obs = env.reset()
-#     one_env.test_case_index = test_case
-#     return init_obs
-
-# def get_hyperparam_dict():
-#     hyperparameters = {
-#         'test_case': test_case,
-#         first_hyperparamter_name: first_hyperparameter,
-#         'epsilon_scalar_robustness': epsilon_scalar_robustness,
-#         'seed': seed,
-#         'LPFULL': LPFULL
-#     }
-#     return hyperparameters
 
 def main():
     np.random.seed(0)
