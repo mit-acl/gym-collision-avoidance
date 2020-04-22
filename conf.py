04b3c60b4f5c3d2829fc738f85d55c995fb095e8
Michael Everett <mfe@mit.edu>
2020-04-22 11:28:59 -0400

    updated ext

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', "sphinx_rtd_theme", 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', "sphinx_rtd_theme", 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
